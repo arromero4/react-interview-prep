b1419e6b9000f5ef5be79c1c5b45a9e5a27c2d50
arromero4 <arromero0408@gmail.com>
2022-09-18 04:09:29 -0500

    React Interview Preparation

--- src/components/RestApiRequest.jsx
+++ src/components/RestApiRequest.jsx
@@ -1,38 +1,105 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-
 export default function RestApiRequest() {
-const [pokemon, usePokemon] = useState([])
+  //const [pokemon, usePokemon] = useState([])
+  const [name, setName] = useState("");
+  const [users, setUsers] = useState([]);
+  //FETCH
+  // useEffect(() =>{const getPokemon = async () => {
+  //   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/`)
+  //   const pokeData = await response.json()
+  //   usePokemon(pokeData.results)
+  // }
+  //   getPokemon()
+  // },[])
 
-//FETCH
-// useEffect(() =>{const getPokemon = async () => {
-//   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/`)
-//   const pokeData = await response.json()
-//   usePokemon(pokeData.results)
-// }
-//   getPokemon()
-// },[])
+  //AXIOS
+  // useEffect(() => {
+  //   try {
+  //     axios
+  //     .get(`https://pokeapi.co/api/v2/pokemon/`)
+  //     .then((res) => {
+  //       usePokemon(res.data.results)
+  //     })
 
-//AXIOS
-useEffect(() => {
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }, []);
+
+  //POST
+  const postData = () => {
+    try {
+      axios
+        .post("https://6326cda35731f3db994d64ea.mockapi.io/users", {
+          name: name,
+          age: 27,
+          hobbies: ["bailar", "cantar", "enfermeria", "comer", "viajar"],
+        })
+        .then((response) => console.log(response.data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    //GET
+    try {
+      axios
+        .get("https://6326cda35731f3db994d64ea.mockapi.io/users")
+        .then((response) => setUsers(response.data));
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  //PUT -UPDATE
+  const updateData = (id) => {
+    console.log(id)
+    try {
+      axios
+        .put(`https://6326cda35731f3db994d64ea.mockapi.io/users/${id}`, {
+          name: name,
+          age: 1,
+          hobbies: ["bailar", "comer", "dormir", "jugar", "viajar"],
+        })
+        .then((response) => console.log(response.data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+ //DELETE
+ const deleteData = (id) => {
+  console.log(id)
   try {
     axios
-    .get(`https://pokeapi.co/api/v2/pokemon/`)
-    .then((res) => {
-      usePokemon(res.data.results)
-    })
-    
+      .delete(`https://6326cda35731f3db994d64ea.mockapi.io/users/${id}`)
+      .then((response) => console.log(response.data));
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}, []);
+ };
 
+ const getData = () => {
+  try {
+    axios
+      .get("https://6326cda35731f3db994d64ea.mockapi.io/users")
+      .then((response) => setUsers(response.data));
+  } catch (error) {
+    console.error(error);
+  }
+ }
 
+ useEffect(() => {
+  
+  getData()
+ }, [])
+ 
 
-  return(
+  return (
     <div>
-       <ul>
+      {/* <ul>
         {
           pokemon.map((poke, i) =>
             (
@@ -40,14 +107,39 @@ useEffect(() => {
             )
           )
         }
+      </ul> */}
+      <input
+        type="text"
+        placeholder="name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={postData}>POST DATA</button>
+      <ul>
+        {users.map((user) => (
+          <>
+            <li key={user.id}>
+            {`${user.id}. ${user.name}`}
+            
+            <button 
+            onClick={() => updateData(user.id)}
+            className="btn btn-primary">
+              Update
+            </button>
+            <button 
+            onClick={() => deleteData(user.id)}
+            className="btn btn-danger">
+              Delete
+            </button>
+            </li>
+          </>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 /**
  * Una API, o interfaz de programación de aplicaciones, es un conjunto de reglas que determinan cómo las aplicaciones o los dispositivos pueden conectarse y comunicarse entre sí. Una API REST es una API que se ajusta a los principios de diseño de REST, un estilo de arquitectura también denominado transferencia de estado representacional
  */
-
 
 /*¿Qué es REST?
 La transferencia de estado representacional (REST) es una arquitectura de software que impone condiciones sobre cómo debe funcionar una API. 
